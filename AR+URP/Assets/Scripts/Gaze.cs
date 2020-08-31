82c43127630013166746554ea961a22ca83c9e3b
maximefrnd <maxime.freund@mmibordeaux.com>
2020-08-31 10:18:32 +0200

    first commit

---
+++ AR+URP/Assets/Scripts/Gaze.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+
+    public GameObject robot;
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+            {
+                GameObject go = hit.collider.gameObject;
+                if (go = robot)
+                    {
+                        CallRobot();
+                    }
+            }
+    }
+
+    void CallRobot(){
+        robot.GetComponent<FaceCamera>().Face();
+        robot.GetComponent<Talk>().talking();
+    }
+}
---
+++ AR+URP/Assets/Scripts/Talk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talk : MonoBehaviour
+{
+
+   public Text uiText;
+   Animator animator;  
+
+    void Start()
+    {
+        uiText.text = "";
+        animator = GetComponent<Animator>();
+    }
+
+    public void talking(){
+        animator.SetBool("IsTalking",true);
+        uiText.text = "Hello ! I'm a robot";
+    }
+}
